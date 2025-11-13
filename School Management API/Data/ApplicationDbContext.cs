2239bd8ceed13639645d8d6ccfad930247462cc7
cyrus alde <aldecyrus084@gmail.com>
2025-11-13 18:00:15 +0800

    Application Db Context Setup

---
+++ School Management API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace School_Management_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
